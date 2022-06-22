d08a160c1ab037dc2572fa8c7a8452d6ac91f246
Pratik <pratik.iosdeveloper@gmail.com>
2022-06-22 20:33:18 +0530

    Create the StoreItem model

---
+++ EcommerceApp/EcommerceApp/StoreItem.swift
@@ -0,0 +1,16 @@
+//
+//  StoreItem.swift
+//  EcommerceApp
+//
+//  Created by Pratik on 22/06/22.
+//
+
+import Foundation
+
+struct StoreItem {
+    let id: String
+    let name: String
+    let address: String
+    let phone: String
+    let website: String
+}
