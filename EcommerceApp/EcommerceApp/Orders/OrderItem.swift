0a128e06a3fa4423ce002c10d7471389212dff84
Pratik <pratik.iosdeveloper@gmail.com>
2022-06-22 23:27:31 +0530

    Make Order: Codable

--- EcommerceApp/EcommerceApp/Orders/OrderItem.swift
+++ EcommerceApp/EcommerceApp/Orders/OrderItem.swift
@@ -7,14 +7,22 @@
 
 import Foundation
 
-struct Order {
+struct Order: Codable {
     let items: [OrderItem]
     let totalPrice: String
     let deliveryAddress: String
+    
+    enum CodingKeys: String, CodingKey {
+        case items, totalPrice, deliveryAddress
+    }
 }
 
-struct OrderItem {
+struct OrderItem: Codable {
     let id: String
     let name: String
     let price: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, price
+    }
 }
