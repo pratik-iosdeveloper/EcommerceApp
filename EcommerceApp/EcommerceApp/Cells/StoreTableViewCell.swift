e966146c016a6faf0d3c9bccd66b74177576d91e
Pratik <pratik.iosdeveloper@gmail.com>
2022-06-23 20:32:21 +0530

    Add store table view cell to the storyboard

---
+++ EcommerceApp/EcommerceApp/Cells/StoreTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StoreTableViewCell.swift
+//  EcommerceApp
+//
+//  Created by Pratik on 23/06/22.
+//
+
+import UIKit
+
+class StoreTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var websiteLabel: UILabel!
+    
+}
