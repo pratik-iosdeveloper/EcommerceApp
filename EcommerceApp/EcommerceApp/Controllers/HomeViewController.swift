b7f9eaa44fa701cedd0c08630bb9e92f90d6aaaa
Pratik <pratik.iosdeveloper@gmail.com>
2022-06-23 16:38:43 +0530

    Add Home, OrderSummary and Success view controllers to the group

---
+++ EcommerceApp/EcommerceApp/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  EcommerceApp
+//
+//  Created by Pratik on 23/06/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ EcommerceApp/EcommerceApp/Controllers/SuccessViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SuccessViewController.swift
+//  EcommerceApp
+//
+//  Created by Pratik on 23/06/22.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
